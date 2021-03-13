2a6b1227b39ef5089498df9ed7a9450aef3fc9a8
Gam3t3ch <gam3t3ch@gmail.com>
2021-03-12 23:51:04 -0800

    added 2 dht11 projects

---
+++ DHT11wLEDStatus.py
@@ -0,0 +1,36 @@
+## will have to run  sudo pip3 install Adafruit_DHT  in terminal.
+import Adafruit_DHT
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(18, GPIO.OUT)
+
+DHT_SENSOR = Adafruit_DHT.DHT11
+DHT_PIN = 17  ##GPIO17 or Pin 11
+
+
+while True:
+    humidity, temperature = Adafruit_DHT.read(DHT_SENSOR, DHT_PIN)
+    if humidity is None or temperature is None:
+        print("Sensor failure. Check wiring.");
+        GPIO.output(18, GPIO.HIGH)
+        time.sleep(5)
+        GPIO.output(18, GPIO.LOW)
+
+    else:
+        print("Temp={0:0.1f}C  Humidity={1:0.1f}%".format(temperature, humidity))
+    time.sleep(6);
+   # if temperature is
+    GPIO.output(18, GPIO.HIGH)
+    time.sleep(.5)
+    GPIO.output(18, GPIO.LOW)
+    time.sleep(.5)
+    GPIO.output(18, GPIO.HIGH)
+    time.sleep(.5)
+    GPIO.output(18, GPIO.LOW)
+    time.sleep(.5)
+
+
+
--- README.md
+++ README.md
@@ -4,4 +4,5 @@ Episodes for Programming on the Raspberry Pi with Pycharm
 
 
 Episodes Guide:</br>
-Episode 1 = ledonoff.py     https://youtu.be/V7nT7u4_yus
+Episode 1 = ledonoff.py     https://youtu.be/V7nT7u4_yus </br>
+Episode 2 = DHT11.py        ----URL------
